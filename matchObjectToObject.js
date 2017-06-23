349209df5067f2793b1b4c0b4d6563cbaee80617
nckro <brainwashedbyme@yahoo.com>
2017-06-23 10:21:25 -0700

    Update matchObjectToObject.js

--- matchObjectToObject.js
+++ matchObjectToObject.js
@@ -1,22 +1,22 @@
 var users = [
- { id: 1, admin: true, level: 1 },  
- { id: 2, admin: true, level: 2 },
- { id: 3, admin: false, level: 1 },
- { id: 4, admin: true, level: 3 },
- { id: 5, admin: true, level: 1 },
+	{ id: 1, admin: true, level: 1 },  
+ 	{ id: 2, admin: true, level: 2 },
+ 	{ id: 3, admin: false, level: 1 },
+ 	{ id: 4, admin: true, level: 3 },
+ 	{ id: 5, admin: true, level: 1 },
 ];
 
 function matchUserToProp(user,prop) {
 	var response = true;
-  Object.keys(prop).forEach(function(key){
-  	response = response && (user[key] && user[key]==prop[key]);
-  });
-  return response;
+	Object.keys(prop).forEach(function(key){
+  		response = response && (user[key] && user[key]==prop[key]);
+ 	});
+  	return response;
 } 
 
 function  getUserWithProp(userList, prop){
     return userList.filter(function (user) {
-        return matchUserToProp(user,prop);
+       	return matchUserToProp(user,prop);
     });
 }
 
