d83c2f199cff61cc97ed8e5f1db94451076d69de
nckro <ioan.barbu420@gmail.com>
2017-06-23 11:52:11 -0700

    upd

--- lab-demos/matchObjectToObject.js
+++ lab-demos/matchObjectToObject.js
@@ -1,5 +1,5 @@
 var users = [
-	{ id: 1, admin: true, level: 1 },  
+	{ id: 1, admin: true, level: 1 },
  	{ id: 2, admin: true, level: 2 },
  	{ id: 3, admin: false, level: 1 },
  	{ id: 4, admin: true, level: 3 },
@@ -12,7 +12,7 @@ function matchUserToProp(user,prop) {
   		response = response && (user[key] && user[key]==prop[key]);
  	});
   	return response;
-} 
+}
 
 function  getUserWithProp(userList, prop){
     return userList.filter(function (user) {
@@ -23,3 +23,21 @@ function  getUserWithProp(userList, prop){
 var filteredUsers = getUserWithProp(users, {admin: true, level: 1});
 console.log(filteredUsers);
 
+var ladders = [
+  { id: 1, height: 20 },
+  { id: 3, height: 25 }
+];
+
+
+function  findWhere(array, criteria){
+  	function matchUserToProp(user,criteria) {
+			var response = true;
+			Object.keys(criteria).forEach(function(key){
+  			response = response && (user[key] && user[key]==criteria[key]);
+ 			});
+  		return response;
+		}
+    return array.filter(function (user) {
+       	return matchUserToProp(user,criteria);
+    })[0].id;
+}
